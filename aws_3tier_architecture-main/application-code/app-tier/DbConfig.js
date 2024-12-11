da1a2e187cc1005a2d195836349ac62e19f6830a
Dhavanisha jegannathan <dhavanisha.2000@gmail.com>
2024-12-11 12:52:57 +0530

    Add files via upload

---
+++ aws_3tier_architecture-main/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cxsiuc4uc98l.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'dhavanisha123',
+    DB_DATABASE : 'webappdb'
+});
