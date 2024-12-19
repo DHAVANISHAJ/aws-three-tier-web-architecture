b1b45a6f09b6d6146267f870bdfab180ca836acd
Dhavanisha jegannathan <dhavanisha.2000@gmail.com>
2024-12-19 10:08:45 +0530

    Delete aws_3tier_architecture-main/application-code directory

--- aws_3tier_architecture-main/application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.cxsiuc4uc98l.us-east-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'dhavanisha123',
-    DB_DATABASE : 'webappdb'
-});
